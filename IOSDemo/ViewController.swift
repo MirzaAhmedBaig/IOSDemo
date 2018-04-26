2c35f2b61b1cecfa0fce99df60c2bec2cda1427b
mirzaahmedbaig <mirza@avantari.org>
2018-04-26 11:54:13 +0530

    Initial Commit

---
+++ IOSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSDemo
+//
+//  Created by Mirza Ahmed Baig on 26/04/18.
+//  Copyright © 2018 Avantari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
