12d542756b05263d963ca7e4a53d0d302d0be43f
mirzaahmedbaig <mirza@avantari.org>
2019-05-21 12:03:24 +0530

    update

--- IOSDemo/GuessGameViewController.swift
+++ IOSDemo/GuessGameViewController.swift
@@ -10,26 +10,26 @@ import UIKit
 
 class GuessGameViewController: UIViewController {
 
+    @IBOutlet var inputFingersCount: UITextField!
+    @IBOutlet var showResultText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func onGuessClicked(_ sender: UIButton) {
+        let holdingCount = arc4random_uniform(6)
+        guard let textFieldString = inputFingersCount.text, let guessedCount = Int(textFieldString) else{
+            return
+        }
+        if guessedCount == holdingCount {
+            showResultText.text = "Yes!, It is Prime number"
+        } else {
+            showResultText.text = "Wrong!, Not a prime number"
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- IOSDemo/ViewController.swift
+++ IOSDemo/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var inputAge: UITextField!
+    
+    @IBOutlet var outputAge: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        guard let textFieldString = inputAge.text, let inputInt = Int(textFieldString) else {
+            return
+        }
+        
+        
+        outputAge.text = "\(inputInt * 7)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
